f33d02bcb8e28ae7f36a015a94ab898c2fad3bed
Arullaja <89208273+Arullaja@users.noreply.github.com>
2022-03-27 22:46:27 +0700

    Update tiktok.js

--- plugins/tiktok.js
+++ plugins/tiktok.js
@@ -1,14 +1,12 @@
-let fetch = require('node-fetch')
-let fs = require('fs')
-let handler = async(m, { conn, usedPrefix, text, command }) => {
-    if (!text) throw `Harap masukkan URL sebagai parameter.\n\nContoh: ${usedPrefix + command} https://vt.tiktok.com/ZSeSCAN1W/`
-    let res = await fetch(global.API('rey', '/api/download/tiktok', { url: text }, 'apikey'))
-    if (!res.ok) throw await `${res.status} ${res.statusText}`
-    let json = await res.json()
-    await conn.sendButtonVid(m.chat, json.result.nowatermark, 'Nih Kak', watermark, 'Thanks', `Thanks`, m)
-}
-handler.command = /^tiktok$/i
-handler.tags = ['downloader']
-handler.help = ['tiktok']
-handler.limit = true
-module.exports = handler
+case 'tiktok':
+	 case 'tiktokdl':
+if (!isUser) return sendButMessage(from, a, b, ton,{quoted : freply}) 
+ 
+ if (isLimit(sender, isPremium, isOwner, limitCount, limit)) return reply(mess.limit)
+				  if (args.length < 1) return reply('*Masukan judul nya?*')
+                reply('Loading.... ')
+				play = args.join(" ")
+				anu = await fetchJson(`https://megayaa.herokuapp.com/api/tiktod/?url=${play}`)
+				sendMediaURL(from, anu.result.nowatermark)
+				limitAdd(sender, limit) 
+					break 
